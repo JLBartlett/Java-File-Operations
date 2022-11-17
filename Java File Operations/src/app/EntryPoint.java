b47f455ac3b6a99ce9740a778a46e3aa5da87476
John Bartlett <johnlindseybartlett@outlook.com>
2022-11-17 20:18:52 +1030

    Created Basic Layout

--- Java File Operations/src/app/EntryPoint.java
+++ Java File Operations/src/app/EntryPoint.java
@@ -1,12 +1,8 @@
 package app;
 
-import java.awt.Dimension;
-import java.awt.DisplayMode;
-import java.awt.GraphicsDevice;
-import java.awt.GraphicsEnvironment;
-import java.awt.Toolkit;
+import java.awt.*;
 
-import javax.swing.JFrame;
+import javax.swing.*;
 
 /***
  * User Interface class
@@ -14,6 +10,7 @@ import javax.swing.JFrame;
  *
  */
 class UI extends JFrame{
+	
 	
 	/***
 	 * Setup UI Frame
@@ -30,7 +27,57 @@ class UI extends JFrame{
 	 * Layout all the components on the UI Frame
 	 */
 	private void layoutComponents() {
-		this.setSize(500, 400);
+		this.setLayout(new BorderLayout());
+		
+		JPanel pnlTop = new JPanel();
+		BoxLayout boxLayout = new BoxLayout(pnlTop, BoxLayout.Y_AXIS);
+		pnlTop.setLayout(boxLayout);
+		
+		JPanel pnlFirst = new JPanel(new FlowLayout(FlowLayout.LEFT));
+		JLabel lblSelFolder = new JLabel("Select Folder:");
+		pnlFirst.add(lblSelFolder);
+		JButton btnChooseFolder = new JButton("Choose Folder...");
+		pnlFirst.add(btnChooseFolder);
+		pnlTop.add(pnlFirst);
+		
+		JPanel pnlSecond = new JPanel(new FlowLayout(FlowLayout.LEFT));
+		JLabel lblEntTextRem = new JLabel("Enter Text to Remove:");
+		pnlSecond.add(lblEntTextRem);
+		JTextField txtRemText = new JTextField(10);
+		pnlSecond.add(txtRemText);
+		JButton btnRemText = new JButton("Remove");
+		pnlSecond.add(btnRemText);
+		pnlTop.add(pnlSecond);
+		
+		JPanel pnlThird = new JPanel(new FlowLayout(FlowLayout.LEFT));
+		JButton btnRemNums = new JButton("Remove Numbers");
+		pnlThird.add(btnRemNums);
+		JButton btnDelSel = new JButton("Delete Selected");
+		pnlThird.add(btnDelSel);
+		pnlTop.add(pnlThird);
+		
+		DefaultListModel<String> lstModel = new DefaultListModel<>();
+		lstModel.addElement("Really long file name to test list");
+		for (int i = 0; i < 100; i++) {
+			lstModel.addElement("File " + i);
+		}
+		JList<String> lstFiles = new JList<>(lstModel);
+		JScrollPane scrollPane = new JScrollPane();
+		scrollPane.setViewportView(lstFiles);
+		
+		
+		JPanel pnlBottom = new JPanel(new FlowLayout(FlowLayout.RIGHT));
+		JButton btnClear = new JButton("Clear");
+		pnlBottom.add(btnClear);
+		JButton btnClose = new JButton("Close");
+		pnlBottom.add(btnClose);
+		
+		this.add(pnlBottom, BorderLayout.PAGE_END);
+		
+		this.add(scrollPane, BorderLayout.CENTER);
+		
+		this.add(pnlTop, BorderLayout.PAGE_START);
+		this.pack();
 	}
 	
 	/***
@@ -42,8 +89,6 @@ class UI extends JFrame{
 		int x = (screenSize.width - windowSize.width) / 2;
 		int y = (screenSize.height - windowSize.height) / 2;
 		this.setLocation(x, y );
-		System.out.println(screenSize);
-		System.out.println(windowSize);
 	}
 }
 
